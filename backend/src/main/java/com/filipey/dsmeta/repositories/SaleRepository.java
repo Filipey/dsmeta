e10dc17306af4f339d04b5396f6c4eda4a703bee
Filipe Moura <filipeasm18@gmail.com>
2022-07-13 17:58:20 -0300

    feat: created SalesRepository

--- backend/src/main/java/com/filipey/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/filipey/dsmeta/repositories/SaleRepository.java
@@ -1,2 +1,8 @@
-package com.filipey.dsmeta.repositories;public interface SaleRepository {
+package com.filipey.dsmeta.repositories;
+
+import com.filipey.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
 }
