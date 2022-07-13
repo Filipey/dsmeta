b826bf1d7ff842578166e34d4c9270c9b9bdc0de
Filipe Moura <filipeasm18@gmail.com>
2022-07-13 18:21:14 -0300

    update: handle minDate and  maxDate

--- backend/src/main/java/com/filipey/dsmeta/services/impl/SalesServiceImpl.java
+++ backend/src/main/java/com/filipey/dsmeta/services/impl/SalesServiceImpl.java
@@ -3,9 +3,13 @@ package com.filipey.dsmeta.services.impl;
 import com.filipey.dsmeta.entities.Sale;
 import com.filipey.dsmeta.repositories.SaleRepository;
 import com.filipey.dsmeta.services.SalesService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SalesServiceImpl implements SalesService {
@@ -17,7 +21,14 @@ public class SalesServiceImpl implements SalesService {
     }
 
     @Override
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
