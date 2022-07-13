98152ad558ca8656c3b9d21136e4fcea54a03a57
Filipe Moura <filipeasm18@gmail.com>
2022-07-13 17:58:49 -0300

    feat: created SalesService implementation

--- backend/src/main/java/com/filipey/dsmeta/services/impl/SalesServiceImpl.java
+++ backend/src/main/java/com/filipey/dsmeta/services/impl/SalesServiceImpl.java
@@ -1,2 +1,23 @@
-package com.filipey.dsmeta.services.impl;public class SalesServiceImpl {
+package com.filipey.dsmeta.services.impl;
+
+import com.filipey.dsmeta.entities.Sale;
+import com.filipey.dsmeta.repositories.SaleRepository;
+import com.filipey.dsmeta.services.SalesService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalesServiceImpl implements SalesService {
+
+    private final SaleRepository repository;
+
+    public SalesServiceImpl(SaleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Sale> findSales() {
+        return repository.findAll();
+    }
 }
