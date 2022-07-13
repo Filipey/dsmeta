ca4fc7703eb1f0ba8b3847a77f0cf1a80144300c
Filipe Moura <filipeasm18@gmail.com>
2022-07-13 18:19:40 -0300

    update: request minDate and maxDate from params

--- backend/src/main/java/com/filipey/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/filipey/dsmeta/controllers/SaleController.java
@@ -2,11 +2,12 @@ package com.filipey.dsmeta.controllers;
 
 import com.filipey.dsmeta.entities.Sale;
 import com.filipey.dsmeta.services.SalesService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/sales")
@@ -19,7 +20,10 @@ public class SaleController {
     }
 
     @GetMapping
-    public List<Sale> findSales() {
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
