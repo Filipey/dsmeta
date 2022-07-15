106b4ea0b1c67075f1d8a677d00959ba60f551ac
Filipe Moura <filipeasm18@gmail.com>
2022-07-15 17:54:48 -0300

    feat: sending notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from 'axios'
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request'
 
 import './styles.css'
 
-function NotificationButton() {
-  return(
-    <div className="dsmeta-red-btn">
+interface NotificationButtonProps {
+  saleId: number
+}
+
+function handleClickButton(saleId: number) {
+  axios
+    .get(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(res => console.log('ok'))
+}
+
+function NotificationButton({ saleId }: NotificationButtonProps) {
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClickButton(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   )
